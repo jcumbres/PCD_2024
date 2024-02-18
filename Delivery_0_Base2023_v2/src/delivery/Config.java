edb349648a7cb75a28a6e8779ed7383feba30e9e
yiuh <yiuh@yiuh-Cyborg-15-A12UCX>
2024-02-18 11:18:45 +0100

    hola

---
+++ Delivery_0_Base2023_v2/src/delivery/Config.java
@@ -0,0 +1,10 @@
+package delivery;
+public class Config {
+	public final static int modoTraza = 2;				// nivel de profundidad de la traza
+	public final static int numeroRestaurantes = 5;		// número de restaurantes a crear
+	public final static int numeroPedidos = 5;			// número de pedidos por canal a crear
+	public final static int numeroMoteros = 2;			// número de moteros por restaurante a crear
+	public final static int numeroProductos = 3; 		// límite de cantidad de productos a crear en pedido
+	public final static int maximoIdProducto = 5; 		// número máximo de id de producto
+	public final static int maximoPrecioProducto = 5;	// precio máximo de cada producto. 
+}
